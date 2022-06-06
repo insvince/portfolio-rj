d1fcbdcb872cf1d087a2f3bf84f527cf1a2b639c
bapcai <phuochuy1328@gmail.com>
2022-06-06 14:31:09 +0700

    first time

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import GlobalStyle from "./resource/assets/GlobalStyle/";
+
+function App() {
+  return (
+    <GlobalStyle>
+      <div className="App"></div>
+    </GlobalStyle>
+  );
+}
+
+export default App;
---
+++ src/resource/assets/GlobalStyle/index.js
@@ -0,0 +1,7 @@
+import "./App.css";
+
+function GlobalStyle({ children }) {
+  return children;
+}
+
+export default GlobalStyle;
