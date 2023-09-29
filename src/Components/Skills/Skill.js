3e722bbf3bf82bc7829ad2ecf688cd4333bc1805
BuildTools <unconfigured@null.spigotmc.org>
2023-09-29 18:42:29 +0700

    reponsive skillItem

--- src/Components/Skills/Skill.js
+++ src/Components/Skills/Skill.js
@@ -1,13 +1,13 @@
 function Skill({ url, name, status }) {
     return (
         <div
-            className={`w-[150px] mt-12 text-center float-left  ${
+            className={`w-1/5 mt-12 text-center float-left duration-300 md:w-1/4 sm:w-1/3 ${
                 status === 'true' && 'animate-translateXReverse'
             }`}>
             <div className="w-full flex justify-center">
-                <img className="w-9 mb-5 sm:mr-2" src={url} alt={name} />
+                <img className="w-9 mb-5 sm:mb-2" src={url} alt={name} />
             </div>
-            <span className="text-xl font-semibold uppercase mr-5 w-[150px] duration-500 xl:text-lg md:text-base sm:w-[150px] sm:text-sm ">
+            <span className="text-xl font-semibold uppercase w-[150px] duration-500 xl:text-lg md:text-base sm:text-sm md:max-w-full ">
                 {name}
             </span>
         </div>
